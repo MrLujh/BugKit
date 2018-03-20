2c83f41101d53dd7f7729905eecf2efcbefa1632
MrLujh <287929070@qq.com>
2018-03-20 15:30:28 +0800

    添加文件

---
+++ BugKit/AppInstallerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppInstallerViewController.h
+//  BugKitExample
+//
+//  Created by lujh on 2018/3/20.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppInstallerViewController : UIViewController
+
+@end
---
+++ BugKit/AppInstallerViewController.m
@@ -0,0 +1,37 @@
+//
+//  AppInstallerViewController.m
+//  BugKitExample
+//
+//  Created by lujh on 2018/3/20.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import "AppInstallerViewController.h"
+
+@interface AppInstallerViewController ()
+
+@end
+
+@implementation AppInstallerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
