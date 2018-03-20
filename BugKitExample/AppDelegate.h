e86d643a4d6343490da4bfb50c6e230e5864ce02
daniulaolu <287929070@qq.com>
2018-03-20 14:53:44 +0800

    添加工程文件

---
+++ BugKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BugKitExample
+//
+//  Created by lujh on 2018/3/20.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BugKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BugKitExample
+//
+//  Created by lujh on 2018/3/20.
+//  Copyright © 2018年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
